a22bdf620725ad7276ead007f328b3a23ba9b293
Jonathan Birge <birge@mit.edu>
2023-12-15 19:47:53 -0500

    remove redundant trace output

--- runtrace.sh
+++ runtrace.sh
@@ -10,7 +10,6 @@ LOCK_FILE="/tmp/trace_output_$UNIQUE_ID.lock"
 touch "$LOCK_FILE"
 
 # Write results to temporary file
-echo "Traceroute to $TARGET_HOST" > $OUT_FILE
 tcptraceroute -f 3 $TARGET_HOST >> $OUT_FILE
 
 # Delete the lock file to indicate completion
